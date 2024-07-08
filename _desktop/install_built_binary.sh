8841c43c6f0d75a30d4676718adc90c0dfd0592e
Joakim von Kistowski <joakimvk@gmail.com>
2024-07-08 19:18:45 +0200

    fixed logo install script

--- _desktop/install_built_binary.sh
+++ _desktop/install_built_binary.sh
@@ -4,12 +4,13 @@ if [ -f steamdeckupdater ]; then
     mkdir -p ~/.local/bin
     mkdir -p ~/.local/share/applications
     mkdir -p ~/.local/share/icons/hicolor/scalable/apps
+    mkdir -p ~/.local/share/icons/hicolor/48x48/apps
     cp -f steamdeckupdater ~/.local/bin/
     cp -f _desktop/steamdeckupdater.desktop ~/.local/share/applications/
     cp -f _desktop/steamdeckupdater-logo.svg ~/.local/share/icons/hicolor/scalable/apps/steamdeckupdater.svg
+    cp -f _desktop/steamdeckupdater-logo.png ~/.local/share/icons/hicolor/48x48/apps/steamdeckupdater.png
     update-desktop-database ~/.local/share/applications
+    echo "Success! Have fun updating your Steam Deck!"
 else
     echo "steamdeckupdater binary not found. Run this in the steamdeck-updater root directory after building."
 fi
-
-
